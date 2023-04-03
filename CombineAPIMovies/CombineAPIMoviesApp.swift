923ce765f726b882a2daf21ff7121efb1d3a0f2f
DGF <daniella.freyre.90@gmail.com>
2023-04-02 23:18:09 -0400

    Initial Commit

---
+++ CombineAPIMovies/CombineAPIMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAPIMoviesApp.swift
+//  CombineAPIMovies
+//
+//  Created by DGF on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineAPIMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
